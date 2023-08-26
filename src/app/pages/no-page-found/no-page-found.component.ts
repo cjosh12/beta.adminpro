502c36e358ffa6c6af1cf56cd8eab1be3bbc6fdc
cjosh12 <carloscalderon826@gmail.com>
2023-08-25 20:07:16 -0600

    changing footers, adding no-page-found

--- src/app/pages/no-page-found/no-page-found.component.ts
+++ src/app/pages/no-page-found/no-page-found.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-no-page-found',
   templateUrl: './no-page-found.component.html',
-  styles: [
+  styleUrls: ['./no-page-found.component.css'
   ]
 })
 export class NoPageFoundComponent {
+  //forma tradicional
+  //year = new Date().getFullYear();
 
+  //signal
+  public year = signal<number>(new Date().getFullYear());
 }
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   ]
 })
 export class FooterComponent {
-
+  public year = signal<number>(new Date().getFullYear())
 }
